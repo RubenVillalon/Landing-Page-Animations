658e0b4b99ca128c90fdc6cca954deda74b4e2c1
Ruben Villalon <rubenvillalond@gmail.com>
2019-07-10 17:58:07 +0200

    navigation css in place for now

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,44 +1,34 @@
-import React, { useState} from "react";
+import React, {useState} from "react";
 import "./index.scss";
-import { useSpring, animated } from "react-spring";
-
+import {useSpring, animated} from "react-spring";
+import logo from './assets/logo.png'
 
 const Navigation = () => {
-  const [ toggle, setToggle] = useState(false);
+    const [toggle, setToggle] = useState(false);
 
-  const animation = useSpring({
-    vector: [0, 10, 30],
-    display: 'block',
-    paddingLeft: 20,
-    background: 'linear-gradient(to right, #009fff, #ec2f4b)',
-    transform: 'translate3d(0px,0,0) scale(1) rotateX(0deg)',
-    boxShadow: '0px 10px 20px 0px rgba(0,0,0,0.4)',
-    borderBottom: '10px solid #2D3747',
-    shape: 'M20,20 L20,380 L380,380 L380,20 L20,20 Z',
-    textShadow: '0px 5px 15px rgba(255,255,255,0.5)'
-  });
+    const NavBarAnimationItems = useSpring({
+        fontSize: toggle ? "30px" : "15px"
+    });
 
-  const NavBarAnimationItems = useSpring({
-    fontSize: toggle ? "30px": "15px"
-  });
+    const InitialNavBar = useSpring({
+        to: {opacity: 1},
+        config: {duration: 3000},
+        delay: {duration: 2000}
+    });
 
-  const InitialNavBar = useSpring({
-    to: { opacity: 1 },
-    config: { duration: 3000 },
-    delay : { duration: 2000 }
-  });
-
-  const number = useSpring({ number: 1000, from: { number: 0 } });
-  
-  return (
-    <animated.div style={InitialNavBar} className="navigation-main-container">
-      <animated.p>{number.number}</animated.p>
-      <animated.p onClick={() => setToggle(!toggle)} style={NavBarAnimationItems}>Home</animated.p>
-      <animated.p>About</animated.p>
-      <animated.p>Gallery</animated.p>
-      <animated.p>Contact</animated.p>
-    </animated.div>
-  );
+    return (
+        <div style={InitialNavBar} className="navigation-container">
+            <div className='left'>
+                <img className='logo' src={logo} alt='logo'/>
+            </div>
+            <div className='right'>
+                <p  className='navigation-items'>Company</p>
+                <p className='navigation-items'>Features</p>
+                <p className='navigation-items'>Contact</p>
+                <button className='register-button'>Register</button>
+            </div>
+        </div>
+    );
 };
 
 export default Navigation;
