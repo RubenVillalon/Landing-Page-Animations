c249af8bc6ea9feeb91f810dfd4d5a94b8b3467b
Ruben Villalon <rubenvillalond@gmail.com>
2019-07-13 09:06:35 +0200

    some updates in the app

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Map = () => {
     return (
         <div>
-            <p><iframe
+            <p><iframe title='map-ruben'
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4542.670535964336!2d8.513907014379916!3d47.39071465996294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900a47300341ff%3A0x5396998960e8deb7!2sETH+Zurich!5e0!3m2!1sen!2sch!4v1562939731254!5m2!1sen!2sch"
     width="100" height="200" allowFullScreen/></p>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import Home from './components/Home';
 import {Provider} from "react-redux";
 import store from "./store";
 import {BrowserRouter} from "react-router-dom";
-import {Switch} from "react-router";
 import Routes from "./Routes";
 
 
