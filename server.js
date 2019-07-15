5a41168fd0bf09bbf1d7d21e5c3bb497951a695e
Ruben Villalon <rubenvillalond@gmail.com>
2019-07-15 14:43:38 +0200

    update tthe server with some more endpoint

--- server.js
+++ server.js
@@ -26,39 +26,44 @@ app.get("/api/users", (req, res) => {
 // get a specific user
 app.get("/api/users/:id", (req, res) => {
   const UserRequested = users.find(user => user.id === parseInt(req.params.id));
-  if (!UserRequested) res.status(404).send("The user does not exist");
+  if (!UserRequested) return res.status(404).send("The user does not exist");
   res.send(UserRequested);
 });
 
 // create a new user
 app.post("/api/users", (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = {
+    id: Math.floor(Math.random()*100),
+    name: req.body.name
   };
 
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  users.push(user);
+  res.send(`your just create the new user: ${user.name} with id: ${user.id}`);
 });
 
 //update a user
-app.get("/api/users/:id", (req, res) => {
+app.put("/api/users/:id", (req, res) => {
   const user = users.find(user => user.id === parseInt(req.params.id));
-  if (!user) res.status(404).send("The user does not exist");
+  if (!user) return res.status(404).send("The user does not exist");
 
   const { error } = validateUser(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   user.name = req.body.name;
   res.send(user);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send("The user does not exist");
+
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+
+  res.status(200).send(`you just deleted the user ${user.id}`);
 });
 
 //getting both integers in the URL into an object with the name properties given in the url path
@@ -71,6 +76,7 @@ app.get("/api/projects/:id/:age", (req, res) => {
   // { sortBy": "name" } in browser
 });
 
+// ~~~~~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 function validateUser(user) {
   const schema = {
     name: Joi.string()
@@ -81,6 +87,7 @@ function validateUser(user) {
   return Joi.validate(user, schema);
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PORTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 const Port = process.env.Port || 4000;
 // if I want to set a Port Environment Variable:
 // $ export PORT=5000 or any port number.
