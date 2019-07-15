f4976dee38de1d0974fec28b32d213a8f6526905
Ruben Villalon <rubenvillalond@gmail.com>
2019-07-15 13:49:41 +0200

    adding more empoints asdding a functio to handle the validations

--- server.js
+++ server.js
@@ -1,46 +1,87 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+const Joi = require("@hapi/joi");
+
+app.use(express.json());
 
 const users = [
-    { id: 1, name: 'User 1' },
-    { id: 2, name: 'User 2' },
-    { id: 3, name: 'USer 3' },
-]
+  { id: 1, name: "User 1" },
+  { id: 2, name: "User 2" },
+  { id: 3, name: "USer 3" }
+];
 
-app.get('/', (req, res ) => {
-    res.send('this is home component')
-})
+app.get("/", (req, res) => {
+  res.send("this is home component");
+});
 
-app.get('/login', (req, res) => {
-    res.send([1,2,3])
-})
+app.get("/login", (req, res) => {
+  res.send([1, 2, 3]);
+});
 
-app.get('/api/users', (req, res) => {
-    res.send(users)
-})
+// get all users
+app.get("/api/users", (req, res) => {
+  res.send(users);
+});
 
+// get a specific user
+app.get("/api/users/:id", (req, res) => {
+  const UserRequested = users.find(user => user.id === parseInt(req.params.id));
+  if (!UserRequested) res.status(404).send("The user does not exist");
+  res.send(UserRequested);
+});
 
-app.get('/api/users/:id', (req, res) => {
-    const UserRequested = users.find(user => user.id === parseInt(req.params.id) )
-    if (!UserRequested) res.status(404).send("The user does not exist")
-    res.send(UserRequested)
-})
+// create a new user
+app.post("/api/users", (req, res) => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
 
+  const result = Joi.validate(req.body, schema);
 
-app.get('/api/users/:id/:age', (req, res) => {
-    res.send(req.params)
-})
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+});
 
-app.get('/api/projects/:id/:age', (req, res) => {
-    res.send(req.query)
-    // { sortBy": "name" } in browser
+//update a user
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+  if (!user) res.status(404).send("The user does not exist");
 
-})
+  const { error } = validateUser(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
 
+  user.name = req.body.name;
+  res.send(user);
+});
 
+//getting both integers in the URL into an object with the name properties given in the url path
+app.get("/api/users/:id/:age", (req, res) => {
+  res.send(req.params);
+});
 
+app.get("/api/projects/:id/:age", (req, res) => {
+  res.send(req.query);
+  // { sortBy": "name" } in browser
+});
 
-const Port = process.env.Port || 4000
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+const Port = process.env.Port || 4000;
 // if I want to set a Port Environment Variable:
 // $ export PORT=5000 or any port number.
-app.listen(Port, () => console.log(`Listening on port ${Port}...`))
+app.listen(Port, () => console.log(`Listening on port ${Port}...`));
