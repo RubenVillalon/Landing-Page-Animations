2a4343ea85e7c3df8848b9b1a9f34d77fa56697f
Ruben Villalon <rubenvillalond@gmail.com>
2019-07-22 13:11:08 +0200

    registration page in place

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -1,33 +1,51 @@
 import React from "react";
-import logo_icon from "./Assets/Logo_Icon.png";
+import logo2 from "../../Assets/Logo2.png";
 import "./index.scss";
 
 const Registration = () => {
   return (
-    <div>
-      <img className="logo-icon" src={logo_icon} alt="logo-chemalive" />
+    <div className="registration-main-container">
+
+      <div className="left-conatiner">
+        
+        
+        <p>ConstruQt - Molecular design tool for high throughput quantum</p>
+        
+      </div>
+
+      <div className="right-container">
+      <img className="logo-icon" src={logo2} alt="logo-chemalive" />
       <p>Registration</p>
-      <div className="registration-form-container">
-        <div className="group">
-          <input type="text" id="name" required="required" />
-          <label htmlFor="name">Name</label>
-          <div className="bar" />
+        <div className="registration-form-container">
+        
+          <div className="input-item">
+            <input type="text" id="name" required="required" />
+            <label>Name</label>
+            <div className="bar" />
+          </div>
+          <div className="input-item">
+            <input type="text" id="name" required="required" />
+            <label>Last Name</label>
+            <div className="bar" />
+          </div>
+          <div className="input-item">
+            <input type="text" id="name" required="required" />
+            <label>Email</label>
+            <div className="bar" />
+          </div>
+          <div className="input-item">
+            <input type="text" id="name" required="required" />
+            <label>Password</label>
+            <div className="bar" />
+          </div>
+          <div className="input-item">
+            <input type="text" id="name" required="required" />
+            <label>Confirm Password</label>
+            <div className="bar" />
+          </div>
+          
         </div>
-        <div className="group">
-          <input type="text" id="name" required="required" />
-          <label htmlFor="name">Last Name</label>
-          <div className="bar" />
-        </div>
-        <div className="group">
-          <input type="text" id="name" required="required" />
-          <label htmlFor="name">Email</label>
-          <div className="bar" />
-        </div>
-        <div className="group">
-          <input type="text" id="name" required="required" />
-          <label htmlFor="name">Password</label>
-          <div className="bar" />
-        </div>
+        <button>Submit</button>
       </div>
     </div>
   );
